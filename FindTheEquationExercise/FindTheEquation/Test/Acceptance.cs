1cf1c98122c4e0fd96333197e10eed5f5877caf0
Marco <mconsola@gmail.com>
2018-09-05 10:34:07 +0100

    acceptance scenario defined

---
+++ FindTheEquationExercise/FindTheEquation/Test/Acceptance.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace FindTheEquation.Test
+{
+    [TestFixture]
+    public class Acceptance
+    {
+        [Test]
+        public void Scenario_50()
+        {
+            Given_The_Digits("123456789");
+
+            When_Finding_Equations_For(50);
+
+            Then_Outcome_Should_Contain("1 - 2 + 34 + 5 + 6 + 7 + 8 - 9 = 50");
+        }
+
+        private void Given_The_Digits(string setupDigits)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void When_Finding_Equations_For(int inputValue)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Then_Outcome_Should_Contain(string expectedOutcome)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
